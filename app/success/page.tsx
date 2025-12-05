945cd60c38ab801658d44031335a2abdd305a82c
Dat Do-Nguyen <dat.donguyen@edu.sait.ca>
2025-12-05 16:20:27 -0700

    changed success page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -1,9 +1,9 @@
-'use client'
+"use client";
 
-import React from 'react'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-import supabase from '@/utils/supabase/client'
+import React from "react";
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import supabase from "@/utils/supabase/client";
 
 // temporary success page
 
@@ -13,16 +13,30 @@ const success = () => {
   // redirects to menupage after 2 seconds
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('/');
+      router.push("/");
     }, 2000);
 
-    return () => clearTimeout(timer)
-    
+    return () => clearTimeout(timer);
   }, [router]);
 
   return (
-    <div>You have been logged in or Your account has been created.</div>
-  )
-}
+    <div role="alert" className="alert alert-info">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        className="h-6 w-6 shrink-0 stroke-current"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        ></path>
+      </svg>
+      <span>Welcome to Gladiator Burger.</span>
+    </div>
+  );
+};
 
-export default success
+export default success;
